7c208df99d78293c6a37100c9b753fa8b49340e6
wenxiangqin <wenxiangqin.wxq@alibaba-inc.com>
2022-01-17 10:51:50 +0800

    java-information

---
+++ src/main/java/com/wenxiangqin/java/info/JavaInfoApplication.java
@@ -0,0 +1,13 @@
+package com.wenxiangqin.java.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wenxiangqin/java/info/JavaInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenxiangqin.java.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
